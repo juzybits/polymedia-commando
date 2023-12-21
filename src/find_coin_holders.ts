a6620534364ffed83e54f7229988b1fc048b3d95
juzybits <96890594+juzybits@users.noreply.github.com>
2023-12-21 22:31:28 -0100

    find_coin_holders - validate API response

--- src/find_coin_holders.ts
+++ src/find_coin_holders.ts
@@ -1,9 +1,10 @@
 import { writeJsonFile } from './common/file_utils.js';
 
+const IS_DEV = true;
+
 let COIN_TYPE = '';
 let OUTPUT_FILE = './data/find_coin_holders.json';
-// const LIMIT = 999999;
-const LIMIT = 3;
+const LIMIT = IS_DEV ? 3 : 999999;
 
 const USAGE = `
 Find Coin<T> holders using the Suiscan API
@@ -47,18 +48,63 @@ async function main()
     /* Fetch */
 
     const URL_HOLDERS = `https://suiscan.xyz/api/sui-backend/mainnet/api/coins/${COIN_TYPE}/holders?sortBy=AMOUNT&orderBy=DESC&searchStr=&page=0&size=${LIMIT}`;
-    const result = await fetch(URL_HOLDERS)
+    const resp: ApiResponse = await fetch(URL_HOLDERS)
     .then((response: Response) => {
         if (!response.ok)
             throw new Error(`HTTP error: ${response.status}`);
         return response.json();
     })
-    .then((result: any) => {
+    .then((result: ApiResponse) => {
         return result;
     });
 
-    writeJsonFile(OUTPUT_FILE, result.content);
+    const isValidResponse =
+        (resp.first) && // it's the first page
+        (resp.last || IS_DEV) && // and also the last page
+        (resp.totalPages === 1 || IS_DEV) && // this is the only page
+        (!resp.empty) && // the page is not empty
+        (resp.totalElements === resp.numberOfElements || IS_DEV) // all holders are included in this one page
+    ;
+    if (!isValidResponse) {
+        console.error('Error: Unexpected response:\n'+
+            `resp.first: ${resp.first}\n` +
+            `resp.last: ${resp.last}\n` +
+            `resp.totalPages: ${resp.totalPages}\n` +
+            `resp.empty: ${resp.empty}\n` +
+            `resp.totalElements: ${resp.totalElements}\n` +
+            `resp.numberOfElements: ${resp.numberOfElements}`
+        );
+        return;
+    }
+
+    writeJsonFile(OUTPUT_FILE, resp.content);
 }
+
+type ApiResponse = {
+    content: [{
+        address: string;
+        holderName: null | string;
+        holderImg: null | string;
+        amount: number;
+        usdAmount: number;
+        percentage: number;
+        countObjects: number;
+        coinType: string;
+        denom: string;
+    }];
+
+    first: boolean;
+    last: boolean;
+    totalPages: number;
+    empty: boolean;
+    totalElements: number;
+    numberOfElements: number;
+
+    number: number;
+    size: number;
+    sort: any;
+    pageable: any;
+};
 
 main();
 
