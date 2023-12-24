6fc57cb6e9a35ff0b8cd704b8edeb3e5e7c26e9f
juzybits <96890594+juzybits@users.noreply.github.com>
2023-12-24 18:01:41 -0100

    add type to bulksender/config.example.ts

--- src/bulksender/config.example.ts
+++ src/bulksender/config.example.ts
@@ -1,6 +1,6 @@
 /**
  * A list of NORMALIZED (important) addresses to be excluded from the airdrop
  */
-export const excludedOwners = [
+export const excludedOwners: string[] = [
     '0x0120000000000000000000000000000000000000000000000000000000000123', // example
 ];
