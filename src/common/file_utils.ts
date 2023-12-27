b41fe1e35796a41756c9c93258a8a202909ea190
juzybits <96890594+juzybits@users.noreply.github.com>
2023-12-27 20:37:52 -0100

    file_utils - readCsvFile()

--- src/common/file_utils.ts
+++ src/common/file_utils.ts
@@ -36,10 +36,37 @@ export function writeCsvFile(filename: string, data: any[][]): void {
     );
 }
 
-export function readJsonFile(filename: string): any {
+// A generic function to parse CSV lines
+export type ParseCsvLine<T> = (values: string[]) => T | null;
+
+export function readCsvFile<T>(filename: string, parseLine: ParseCsvLine<T>, reverse: boolean = false): T[] {
+    const results: T[] = [];
     const fileContent = fs.readFileSync(filename, 'utf8');
-    const jsonData = JSON.parse(fileContent);
-    return jsonData;
+
+    // Split the content into lines and optionally reverse the array
+    let lines = fileContent.split('\n');
+    if (reverse) {
+        lines = lines.reverse();
+    }
+
+    for (const line of lines) {
+        const trimmedLine = line.trim();
+        if (!trimmedLine) {
+            // console.log('[readCsvFile] Skipping empty line');
+            continue;
+        }
+
+        // Split the line by commas and remove quotes from each value
+        const values = trimmedLine.split(',').map(value =>
+            value.replace(/^"|"$/g, '').replace(/\\"/g, '"').trim()
+        );
+        const parsedLine = parseLine(values);
+        if (parsedLine !== null) {
+            results.push(parsedLine);
+        }
+    }
+
+    return results;
 }
 
 export function writeJsonFile(filename: string, contents: any): void {
@@ -47,4 +74,10 @@ export function writeJsonFile(filename: string, contents: any): void {
         filename,
         JSON.stringify(contents, null, 4)
     );
+}
+
+export function readJsonFile(filename: string): any {
+    const fileContent = fs.readFileSync(filename, 'utf8');
+    const jsonData = JSON.parse(fileContent);
+    return jsonData;
 }
