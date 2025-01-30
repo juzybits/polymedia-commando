4aea8f864db5e265d4b00d0b0b91e3f091037955
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-30 15:32:41 -0100

    refactor bulksend.ts

--- src/cli/src/commands/bulksender/bulksend.ts
+++ src/cli/src/commands/bulksender/bulksend.ts
@@ -7,7 +7,6 @@ import { coinWithBalance, Transaction } from "@mysten/sui/transactions";
 import {
     balanceToString,
     chunkArray,
-    formatNumber,
     NetworkName,
     newSignTx,
     sleep,
@@ -23,14 +22,12 @@ import {
     readCsvFile,
 } from "@polymedia/suitcase-node";
 
-// === config ===
-
 /**
  * The Polymedia Bulksender package ID which contains the bulksender::send() function
  */
 const PACKAGE_IDS = new Map<NetworkName, string> ([
     ["localnet", ""],
-    ["devnet", ""],
+    ["devnet", "0x680147436c5ca7501bdda0bc99625d097d2a7a3db48a79949d4192cb1acea3dc"],
     ["testnet", "0x7f541b25c64aa2d0330a64dfaeb7c0e35924b6633069b8047125e038728d15d6"],
     ["mainnet", "0x026b2b422e630c79c961fdb5d63821547ec8fb2ad5b7095189c440e1bdbba9f1"],
 ]);
@@ -46,158 +43,132 @@ const BATCH_SIZE = 500;
  * https://docs.sui.io/references/sui-api/rpc-best-practices
  */
 const RATE_LIMIT_DELAY = 334;
-/**
- * To estimate total gas costs
- */
-const GAS_PER_ADDRESS = 0.0013092459;
 
-/// === command ===
-
+// TODO: abort if current gas is lower than gas estimate
 export async function bulksend(
     coinType: string,
     inputFile: string,
     outputFile: string,
 ): Promise<void>
 {
+    // === check input and output files ===
+    console.log(`Input file: ${inputFile}`);
+    console.log(`Output file: ${outputFile}`);
+    if (!fileExists(inputFile)) {
+        throw new Error(`${inputFile} doesn't exist. Create a .csv file with two columns: address and amount.`);
+    }
+    if (fileExists(outputFile)) {
+        throw new Error(`${outputFile} already exists. Check and handle it before rerunning the script.`);
+    }
+
+    // === initialize client ===
+    const [networkName, signer] = await Promise.all([
+        getActiveEnv(),
+        getActiveKeypair(),
+    ]);
+    const suiClient = new SuiClient({ url: getFullnodeUrl(networkName)});
+    const client = new SuiClientBaseWrapper({
+        suiClient,
+        signTx: newSignTx(suiClient, signer),
+    });
+    const activeAddress = signer.toSuiAddress();
+    console.log(`Active network: ${networkName}`);
+    console.log(`Active address: ${activeAddress}`);
+
+    // === check coin metadata ===
+    console.log(`Coin type: ${coinType}`);
+    const coinMeta = await suiClient.getCoinMetadata({ coinType });
+    if (!coinMeta) {
+        throw new Error(`Failed to get CoinMetadata for coin type "${coinType}"`);
+    }
+    const coinSymbol = coinMeta.symbol;
+    const coinDecimals = coinMeta.decimals;
+    console.log(`Coin symbol: ${coinSymbol}`);
+    console.log(`Coin decimals: ${coinDecimals}`);
+
+    // === get user balance ===
+    const userBalance = BigInt(
+        (await suiClient.getBalance({ owner: activeAddress, coinType })).totalBalance
+    );
+    console.log(`User balance: ${balanceToString(userBalance, coinDecimals)} ${coinSymbol}`);
+
+    // === read addresses and amounts from input file ===
+    const addrsAndBals = readCsvFile<AddressBalancePair>(inputFile, (vals) => parseCsvLine(vals, coinDecimals));
+    console.log(`Found ${addrsAndBals.length} addresses in ${inputFile}`);
+
+    const batches = chunkArray(addrsAndBals, BATCH_SIZE);
+    console.log(`Airdrop will be done in ${batches.length} transaction blocks`);
+
+    const totalBalance = addrsAndBals.reduce((sum, pair) => sum + pair.balance, BigInt(0));
+    console.log(`Total amount to be sent: ${balanceToString(totalBalance, coinDecimals)} ${coinSymbol}`);
+    if (totalBalance > userBalance) {
+        throw new Error("Total amount to be sent is bigger than user balance");
+    }
+
+    // === get user confirmation ===
+    const userConfirmed = await promptUser("\nDoes this look okay? (y/n) ");
+    if (!userConfirmed) {
+        console.log("Execution aborted by the user.");
+        return;
+    }
+
+    // === send Coin<T> to each address ===
+
+    let totalGas = 0;
+    let batchNumber = 0;
     try {
-        // Check input and output files
-        console.log(`Input file: ${inputFile}`);
-        console.log(`Output file: ${outputFile}`);
-        if (!fileExists(inputFile)) {
-            throw new Error(`${inputFile} doesn't exist. Create a .csv file with two columns: address and amount.`);
-        }
-        if (fileExists(outputFile)) {
-            throw new Error(`${outputFile} already exists. Check and handle it before rerunning the script.`);
-        }
+        const packageId = PACKAGE_IDS.get(networkName);
+        for (const batch of batches)
+        {
+            batchNumber++;
+            const batchBalance = batch.reduce((total, pair) => total + pair.balance, BigInt(0));
+            logTransactionStart(outputFile, batchBalance, batchNumber, batch);
 
-        // Initialize SuiClient
-        const [networkName, signer] = await Promise.all([
-            getActiveEnv(),
-            getActiveKeypair(),
-        ]);
-        const suiClient = new SuiClient({ url: getFullnodeUrl(networkName)});
-        const client = new SuiClientBaseWrapper({
-            suiClient,
-            signTx: newSignTx(suiClient, signer),
-        });
-        const activeAddress = signer.toSuiAddress();
-        console.log(`Active network: ${networkName}`);
-        console.log(`Active address: ${activeAddress}`);
+            const tx = new Transaction();
+            const payCoin = coinWithBalance({
+                type: coinType,
+                balance: batchBalance,
+            });
+            tx.moveCall({
+                target: `${packageId}::bulksender::send`,
+                typeArguments: [ coinType ],
+                arguments: [
+                    payCoin,
+                    tx.pure(bcs.vector(bcs.U64).serialize(
+                        batch.map(pair => pair.balance)
+                    )),
+                    tx.pure(bcs.vector(bcs.Address).serialize(
+                        batch.map(pair => pair.address)
+                    )),
+                ],
+            });
 
-        // Check coin metadata
-        console.log(`Coin type: ${coinType}`);
-        const coinMeta = await suiClient.getCoinMetadata({ coinType });
-        if (!coinMeta) {
-            throw new Error(`Failed to get CoinMetadata for coin type "${coinType}"`);
-        }
-        const coinSymbol = coinMeta.symbol;
-        const coinDecimals = coinMeta.decimals;
-        console.log(`Coin symbol: ${coinSymbol}`);
-        console.log(`Coin decimals: ${coinDecimals}`);
+            const result = await client.signAndExecuteTx(tx);
 
-        // Get user coinType balance
-        const userBalance = BigInt(
-            (await suiClient.getBalance({ owner: activeAddress, coinType })).totalBalance
-        );
-        console.log(`User balance: ${balanceToString(userBalance, coinDecimals)} ${coinSymbol}`);
-
-        // Read addresses and amounts from input file
-        function parseCsvLine(values: string[]): AddressBalancePair | null {
-            const [addressStr, amountStr] = values;
-
-            const address = validateAndNormalizeAddress(addressStr);
-
-            if (address === null) {
-                console.debug(`[parseCsvLine] Skipping line with invalid owner: ${addressStr}`);
-                return null;
+            if (result.effects?.status.status !== "success") {
+                throw new Error(`Transaction status was '${result.effects?.status.status}': ${result.digest}`);
             }
 
-            const balance = stringToBalance(amountStr, coinDecimals);
-
-            return { address, balance };
-        }
-        const addrsAndBals = readCsvFile<AddressBalancePair>(inputFile, parseCsvLine);
-        console.log(`\nFound ${addrsAndBals.length} addresses in ${inputFile}`);
-        const batches = chunkArray(addrsAndBals, BATCH_SIZE);
-        console.log(`Airdrop will be done in ${batches.length} transaction blocks`);
-        console.log(`Gas estimate: ${formatNumber(GAS_PER_ADDRESS*addrsAndBals.length)} SUI`);
-        // TODO: abort if current gas is lower than gas estimate
-        const totalBalance = addrsAndBals.reduce((sum, pair) => sum + pair.balance, BigInt(0));
-        console.log(`Total amount to be sent: ${balanceToString(totalBalance, coinDecimals)} ${coinSymbol}`);
-
-        // Abort if user doesn't have enough balance
-        if (totalBalance > userBalance) {
-            throw new Error("Total amount to be sent is bigger than user balance");
-        }
-
-        // Get user confirmation before proceeding
-        const userConfirmed = await promptUser("\nDoes this look okay? (y/n) ");
-        if (!userConfirmed) {
-            console.log("Execution aborted by the user.");
-            return;
-        }
-
-        /* Send Coin<T> to each address */
-
-        let totalGas = 0;
-        let batchNumber = 0;
-        try {
-            const packageId = PACKAGE_IDS.get(networkName);
-            for (const batch of batches)
-            {
-                batchNumber++;
-                const batchBalance = batch.reduce((total, pair) => total + pair.balance, BigInt(0));
-                logTransactionStart(outputFile, batchBalance, batchNumber, batch);
-
-                const tx = new Transaction();
-                const payCoin = coinWithBalance({
-                    type: coinType,
-                    balance: batchBalance,
-                });
-                tx.moveCall({
-                    target: `${packageId}::bulksender::send`,
-                    typeArguments: [ coinType ],
-                    arguments: [
-                        payCoin,
-                        tx.pure(bcs.vector(bcs.U64).serialize(
-                            batch.map(pair => pair.balance)
-                        )),
-                        tx.pure(bcs.vector(bcs.Address).serialize(
-                            batch.map(pair => pair.address)
-                        )),
-                    ],
-                });
-
-                const result = await client.signAndExecuteTx(tx);
-
-                if (result.effects?.status.status !== "success") {
-                    throw new Error(`Transaction status was '${result.effects?.status.status}': ${result.digest}`);
-                }
-
-                if (result.effects?.created?.length !== batch.length) {
-                    throw new Error(`Transaction created ${result.effects?.created?.length} objects `
-                    + `for ${batch.length} addresses: ${result.digest}`);
-                }
-
-                logText(outputFile, `Transaction successful: ${result.digest}`);
-
-                const gas = result.effects.gasUsed;
-                totalGas += Number(gas.computationCost) + Number(gas.storageCost) - Number(gas.storageRebate);
-
-                // Wait a bit to stay below the public RPC rate limit
-                if (networkName !== "localnet") {
-                    await sleep(RATE_LIMIT_DELAY);
-                }
+            if (result.effects?.created?.length !== batch.length) {
+                throw new Error(`Transaction created ${result.effects?.created?.length} objects `
+                + `for ${batch.length} addresses: ${result.digest}`);
             }
-            console.log("\nDone!");
-            console.log(`Gas used: ${totalGas / 1_000_000_000} SUI\n`);
+
+            logText(outputFile, `Transaction successful: ${result.digest}`);
+
+            const gas = result.effects.gasUsed;
+            totalGas += Number(gas.computationCost) + Number(gas.storageCost) - Number(gas.storageRebate);
+
+            if (networkName !== "localnet") {
+                await sleep(RATE_LIMIT_DELAY); // give the RPC a break
+            }
         }
-        catch (error) {
-            logText(outputFile, String(error));
-            throw(error);
-        }
-    } catch (error) {
-        console.error(error);
+        console.log("\nDone!");
+        console.log(`Gas used: ${totalGas / 1_000_000_000} SUI\n`);
+    }
+    catch (error) {
+        logText(outputFile, String(error));
+        throw error;
     }
 }
 
@@ -209,6 +180,23 @@ type AddressBalancePair = {
 };
 
 // === helpers ===
+
+class SuiClientBaseWrapper extends SuiClientBase {}
+
+function parseCsvLine(
+    values: string[],
+    decimals: number,
+): AddressBalancePair | null
+{
+    const [addrStr, amountStr] = values;
+    const address = validateAndNormalizeAddress(addrStr);
+    if (address === null) {
+        console.debug(`[parseCsvLine] Skipping line with invalid owner: ${addrStr}`);
+        return null;
+    }
+    const balance = stringToBalance(amountStr, decimals);
+    return { address, balance };
+}
 
 /**
  * Output looks like this:
@@ -237,5 +225,3 @@ function logText(
     const logEntry = `${date} - ${text}\n`;
     appendFileSync(outputFile, logEntry);
 }
-
-class SuiClientBaseWrapper extends SuiClientBase {}
